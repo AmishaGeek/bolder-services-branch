8f0e9dd35c4225242a2c9810e0844f1007455a87
AmishaGeek <amishageek435@gmail.com>
2022-10-19 19:14:31 +0530

    19-10-2022

---
+++ template-parts/content-gallery.php
@@ -0,0 +1,24 @@
+
+<section class="main-banner inner-banner rest-banner sec-1920" style=" background-image: url('<?php echo home_url(); ?>/wp-content/uploads/2022/09/banner-img-min.jpg');">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="banner-content white-text text-center">
+                    <h1 class="h1-title wow fadeup-animation" data-wow-duratin="0.8s" data-wow-delay="0.2s"><?php the_title(); ?></h1>
+                </div>
+            </div>
+        </div>
+    </div>
+</section><!-- emd of main-banner -->
+
+<section class="main-gallery">
+    <div class="container">
+        <div class="row">
+            <div class="gallery-img" style="width: 100%; display: inline-block;">
+                <a data-fancybox="Gallery" title="Gallery-7" href="http://192.168.1.18/bolder-services/wp-content/uploads/2022/09/ACphoto-2880w-copiar.jpg" tabindex="-1">
+                    <div class="bg-img" style="background-image: url('http://192.168.1.18/bolder-services/wp-content/uploads/2022/09/ACphoto-2880w-copiar.jpg');"></div>
+                </a>
+            </div>
+        </div>
+    </div>
+</section>
